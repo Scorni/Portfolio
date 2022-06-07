779cd08b66a74904d2d0a6c85dcae9b361044953
Scorni <m.scorniciel@students.ephec.be>
2022-06-07 16:23:53 +0200

    initial commit + started front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/style.css';
+import { NavLink, Link } from "react-router-dom";
+import { Icon } from '@iconify/react';
+
+import Arrow from "./components/arrow";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="siteName">
+          <h1>SCORNI</h1>
+        </div>
+        <div className="navbar">
+          <NavLink className="navlink" to="/">HOME</NavLink>
+          <NavLink className="navlink" to="/about">ABOUT</NavLink>
+          <NavLink className="navlink" to="/experiences">EXPERIENCES</NavLink>
+          <NavLink className="navlink" to="/hitme">HIT ME UP</NavLink>
+        </div>
+      </div>
+      <div className="body">
+        <div className="main">
+          <div className="welcomeText">
+            <h1>Welcome on my portfolio.<br />It means it’s mine.</h1>
+          </div>
+          <div className="book">
+            <Icon className="icon" icon="akar-icons:book-open" />
+          </div>
+          <div className="checkButton">
+            <Arrow className="arrow" />
+            <Link to="/experiences">
+              <button>
+                Check it out
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="side">
+          <div className="informations">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+          <div className="news">
+            <div className="first">
+                <h3>News</h3>
+                <p>07/06/2022</p>
+                <h4>This has been launch on Today</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.. Read More</p>
+            </div>
+            <div className="second">
+              <h3>News</h3>
+              <p>07/06/2022</p>
+              <h4>This has been launch on Today</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.. Read More</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
