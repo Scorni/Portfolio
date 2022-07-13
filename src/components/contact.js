4b5340099ff3eafb36cffa12b232ba40c297c802
Scorni <m.scorniciel@students.ephec.be>
2022-07-13 15:50:56 +0200

    trying fixing trans

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,53 +1,67 @@
 import '../assets/css/contact/contact.css';
+import {  Trans } from "react-i18next";
 
 // Here, we display our Navbar
 export default function Arrow() {
  return (
     <main className="contact">
         <div className="contactText">
-            <h2>
-                Want to ask something ? <br/>
-                Want a new design for your old website ? <br/>
-                Or you just want a new website ? <br/>
-                Need help on your current website ? <br />
-                Need a custom domain name or hosting your application? <br />
-                Don't be afraid of asking me !
-            </h2>
+            <Trans i18nKey="text">
+                <h2>
+                    Want to ask something ? <br/>
+                    Want a new design for your old website ? <br/>
+                    Or you just want a new website ? <br/>
+                    Need help on your current website ? <br />
+                    Need a custom domain name or hosting your application? <br />
+                    Don't be afraid of asking me !
+                </h2>
+            </Trans>
         </div>
         <div className="contactForm">
             <form>
                 <div className='name'>
-                    <label for="name" className="nameLabel">Name</label>
+                    <Trans i18nKey="name">
+                        <label label for="name" className="nameLabel">Name</label>
+                    </Trans>
                     <input placeholder="John" type="text" name="name" className="nameInput"></input>
                 </div>
                 <div className='firstName'>
-                    <label for="firstName" className="firstNameLabel">First Name</label>
+                    <Trans i18nKey="firstName">
+                        <label for="firstName" className="firstNameLabel">First Name</label>
+
+                    </Trans>
                     <input placeholder="Doe" type="text" name="firstName" className="firstNameInput"></input> 
                 </div>
                 <br/>
                 <div className="mail">
-                    <label for="email" className="emailLabel">Email</label>
+                    <Trans i18nKey="email">
+                        <label for="email" className="emailLabel">Email</label>
+                    </Trans>
                     <input placeholder="John@Doe.com"type="email" name="email" className="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required></input>
                 </div>
                 <div className="subject">
-                    <label for="subject" className="subjectLabel">Choose your subject</label>
-                    <select className="subjectLabel" name="subjectLabel" id="pet-select">
-                        <option defaultValue="">Please choose your Subject</option>
-                        <option value="Design">Design</option>
-                        <option value="Price">Price</option>
-                        <option value="Website">Website</option>
-                        <option value="Domain">Domain</option>
-                        <option value="Hosting">Hosting</option>
-                        <option value="Other">Other</option>
-                    </select>
+                    <Trans i18nKey="subject">
+                        <label for="subject" className="subjectLabel">Choose your subject</label>
+                        <select className="subjectLabel" name="subjectLabel" id="pet-select">
+                            <option defaultValue="">Please choose your Subject</option>
+                            <option value="Design">Design</option>
+                            <option value="Price">Price</option>
+                            <option value="Website">Website</option>
+                            <option value="Domain">Domain</option>
+                            <option value="Hosting">Hosting</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </Trans>
                 </div>
                 <div className="question">
-                    <label for="questionTextArea" className="questionLabel">Ask your question below</label>
-                    <textarea className="questionTextArea" name="questionTextArea" form="usrform" placeholder="Write Here !" />
+                    <Trans i18nKey="question">
+                        <label for="questionTextArea" className="questionLabel">Ask your question below</label>
+                    </Trans>
+                    <textarea className="questionTextArea" name="questionTextArea" form="usrform" placeholder="..." />
                 </div>
                 <div className="sendQuestion">
                     <label for="submitQuestion"></label>
-                    <input name="submitQuestion" className="submitQuestion" type="submit" value="Send"></input>
+                    <input name="submitQuestion" className="submitQuestion" type="submit"   ></input>
                 </div>
             </form>
         </div>
